3821285b13ab777317467587fd1dde3144acbf22
kahzmic <159095560+kahzmic@users.noreply.github.com>
2026-02-14 20:07:25 -0600

    just want to get some stuff to another computer that's why I'm pushing

---
+++ src/subsystems/video_streaming/webcam.py
@@ -0,0 +1,44 @@
+import cv2 as cv
+import numpy as np
+
+from src.toolbox.globals import path_to
+from src.subsystems.video_streaming.video_stream import Intrinsics, VideoStream
+
+
+class WebCamVideoStream(VideoStream):
+    """Video stream from a standard USB webcam using OpenCV."""
+
+    def __init__(self, index: int) -> None:
+        """Initialize the webcam video stream."""
+        self.intrinsics: Intrinsics = Intrinsics(
+            np.load(
+                f"{path_to.calibration_presets}/main_sentry_cam/dist.pkl",
+                allow_pickle=True,
+            ),
+            np.load(
+                f"{path_to.calibration_presets}/main_sentry_cam/camera_matrix.pkl",
+                allow_pickle=True,
+            ),
+        )
+        self.cap = cv.VideoCapture(index)  # Use the default webcam
+        if not self.cap.isOpened():
+            raise Exception("Could not open webcam.")
+
+    def get_frame(self):
+        """Return the most recent frame from the webcam."""
+        ret, frame = self.cap.read()
+        if not ret:
+            raise Exception("Could not read frame from webcam.")
+        return frame
+
+    def get_intrinsics(self) -> Intrinsics:
+        """Return camera intrinsics loaded from calibration presets."""
+        return self.intrinsics
+
+    @property
+    def height(self) -> int:
+        return int(self.cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+    @property
+    def width(self) -> int:
+        return int(self.cap.get(cv.CAP_PROP_FRAME_WIDTH))
